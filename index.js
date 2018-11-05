650028d669ddaf60a4987de6fe09c198e468f934
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-11-05 22:01:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solutions in this file
 function printBadges(names)  {
-  for 
+  for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
   
 }
