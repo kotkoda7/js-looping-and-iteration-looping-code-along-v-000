5162ffa1ecab25fa150c52255c7dce354bb8d37f
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-11-05 22:02:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 function printBadges(names)  {
   for (let i = 0; i < names.length; i++) {
-        console.log(`Welcome ${names[i]} and added a bow!`);
+        console.log(`Welcome ${names[i]} You are employee $[i]!`);
   
 }
